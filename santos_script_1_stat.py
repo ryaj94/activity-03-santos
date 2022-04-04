111253d0373c046cbe8abe17c74ddca800d8a1b2
unknown <nctryryaj2694@gmail.com>
2022-04-04 22:06:50 +0800

    last

--- santos_script_1_stat.py
+++ santos_script_1_stat.py
@@ -1,11 +1,9 @@
 class pokestats():
 
     def statReturnHP(base,iv,ev,lvl):
-        #return base+iv+ev+lvl
         return ((((2*base)+iv+(ev/4))*lvl)/100)+lvl+10
 
     def statReturnOtherStat(base,iv,ev,lvl,nature):
-        #return base+iv+ev+lvl
         return (((((2*base)+iv+(ev/4))*lvl)/100)+5)*nature
 
 
